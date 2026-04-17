6f83ab31a694692a4203194400d0eb200168d65b
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-04-17 11:22:57 -0300

    feat(controllers): create zod validation   Added a new pipe that validates the body schema of requests in controllers. For this was needed to install the zod and zod-validation-errors to format.

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,7 +1,17 @@
-import { Body, ConflictException, Controller, HttpCode, Post } from '@nestjs/common'
+import { Body, ConflictException, Controller, HttpCode, Post, UsePipes } from '@nestjs/common'
 import { hash } from 'bcryptjs'
+import z from 'zod'
 
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
 import { PrismaService } from '../prisma/prisma.service'
+
+const createAccountBodySchema = z.object({
+  email: z.email(),
+  name: z.string(),
+  password: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -9,7 +19,8 @@ export class CreateAccountController {
 
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body
 
     const userWithSameEmail = await this.prisma.user.findUnique({
