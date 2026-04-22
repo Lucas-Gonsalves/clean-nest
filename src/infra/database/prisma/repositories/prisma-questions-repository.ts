e54b6ce50562a9971a020c28ab4aea918d50e91d
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-04-22 15:51:40 -0300

    feat(database): update prisma-question-repository   Created a new mapper on database-prisma to format the question from prisma to a domain question.

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -4,10 +4,21 @@ import { PaginationParams } from '@/src/core/repositories/pagination-params'
 import { QuestionsRepository } from '@/src/domain/forum/application/repositories/question-repository'
 import { Question } from '@/src/domain/forum/enterprise/entities/question'
 
+import { PrismaQuestionMapper } from '../mappers/prisma-question-mapper'
+import { PrismaService } from '../prisma.service'
+
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
+  constructor(private prisma: PrismaService) {}
+
   async findById(id: string): Promise<Question | null> {
-    throw new Error('Method not implemented.')
+    const question = await this.prisma.question.findUnique({ where: { id } })
+
+    if (!question) {
+      return null
+    }
+
+    return PrismaQuestionMapper.toDomain(question)
   }
   async create(question: Question): Promise<void> {
     throw new Error('Method not implemented.')
