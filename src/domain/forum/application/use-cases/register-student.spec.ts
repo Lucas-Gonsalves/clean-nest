11d63e6f25a075289555b5ff3cf97daaa347059e
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-04-28 15:30:20 -0300

    feat(forum-use-case): create test to authenticate   Added a new test to the forum use-case, authenticate-student, that will validate the authenticate if it's working correctly.

--- src/domain/forum/application/use-cases/register-student.spec.ts
+++ src/domain/forum/application/use-cases/register-student.spec.ts
@@ -3,6 +3,7 @@ import { beforeEach, describe, expect, it } from 'vitest'
 import { FakeHasher } from '@/test/cryptography/fake-hasher'
 import { InMemoryStudentsRepository } from '@/test/repositories/forum/in-memory-students-repository'
 
+import { StudentAlreadyExistsError } from './errors/student-already-exists-error'
 import { RegisterStudentUseCase } from './register-student'
 
 let inMemoryStudentsRepository: InMemoryStudentsRepository
@@ -27,5 +28,35 @@ describe('Register Student', () => {
     expect(result.value).toEqual({
       student: inMemoryStudentsRepository.items[0],
     })
+  })
+
+  it('should hash studant password upon registration', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const hashedPassword = await fakeHasher.hash('123456')
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryStudentsRepository.items[0].password).toEqual(hashedPassword)
+  })
+
+  it('should not be able to register a new student with an email from another studant', async () => {
+    await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(StudentAlreadyExistsError)
   })
 })
