8414d845ad87e5ba066d0f4dc27df61445dc0adc
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-05-01 14:56:54 -0300

    feat(controllers): create comment-on-answer   Added a new controller to comment in a new answer. It was tested and validated.

--- test/factories/forum/make-answer.ts
+++ test/factories/forum/make-answer.ts
@@ -1,18 +1,22 @@
 import { faker } from '@faker-js/faker'
+
+import { UniqueEntityId } from '@/src/core/entities/unique-entity-id'
+
 import { Injectable } from '@nestjs/common'
-import { UniqueEntityId } from '@src/core/entities/unique-entity-id'
-import { Answer, type AnswerProps } from '@src/domain/forum/enterprise/entities/answer'
-
-import { PrismaAnswerMapper } from '@/src/infra/database/prisma/mappers/prisma-answer-mapper'
 import { PrismaService } from '@/src/infra/database/prisma/prisma.service'
+import { PrismaAnswerMapper } from '@/src/infra/database/prisma/mappers/prisma-answer-mapper'
+import { Answer, AnswerProps } from '@/src/domain/forum/enterprise/entities/answer'
 
-export function makeAnswer(overrride: Partial<AnswerProps> = {}, id?: UniqueEntityId) {
+export function makeAnswer(
+  override: Partial<AnswerProps> = {},
+  id?: UniqueEntityId,
+) {
   const answer = Answer.create(
     {
-      questionId: new UniqueEntityId(),
       authorId: new UniqueEntityId(),
+      questionId: new UniqueEntityId(),
       content: faker.lorem.text(),
-      ...overrride,
+      ...override,
     },
     id,
   )
@@ -27,7 +31,9 @@ export class AnswerFactory {
   async makePrismaAnswer(data: Partial<AnswerProps> = {}): Promise<Answer> {
     const answer = makeAnswer(data)
 
-    await this.prisma.answer.create({ data: PrismaAnswerMapper.toPersistence(answer) })
+    await this.prisma.answer.create({
+      data: PrismaAnswerMapper.toPersistence(answer),
+    })
 
     return answer
   }
