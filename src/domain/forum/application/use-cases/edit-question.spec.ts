d18e4c0b5a03dea4b9650b7ac273b99c5a6732a8
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-05-05 17:18:36 -0300

    feat(forum-use-case): updating edit and create question   Added a method for creating attachments when creating or editing a question in the use case.

--- src/domain/forum/application/use-cases/edit-question.spec.ts
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -76,4 +76,44 @@ describe('Edit Question', () => {
       content: newQuestion.content,
     })
   })
+
+  it('should sync new and remove attachments when editing a question', async () => {
+    const newQuestion = makeQuestion({
+      authorId: new UniqueEntityId('author-1'),
+    })
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    inMemoryQuestionsAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityId('1'),
+      }),
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityId('2'),
+      }),
+    )
+
+    const result = await sut.execute({
+      questionId: newQuestion.id.toString(),
+      authorId: 'author-1',
+      title: 'Question updated title',
+      content: 'Question updated content',
+      attachmentsIds: ['1', '3'],
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryQuestionsAttachmentsRepository.items).toHaveLength(2)
+    expect(inMemoryQuestionsAttachmentsRepository.items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          attachmentId: new UniqueEntityId('1'),
+        }),
+        expect.objectContaining({
+          attachmentId: new UniqueEntityId('3'),
+        }),
+      ]),
+    )
+  })
 })
--- test/repositories/forum/in-memory-question-attachments-repository.ts
+++ test/repositories/forum/in-memory-question-attachments-repository.ts
@@ -4,6 +4,18 @@ import type { QuestionAttachment } from '@src/domain/forum/enterprise/entities/q
 export class InMemoryQuestionsAttachmentsRepository implements QuestionsAttachmentsRepository {
   public items: QuestionAttachment[] = []
 
+  async createMany(attachments: QuestionAttachment[]) {
+    this.items.push(...attachments)
+  }
+
+  async deleteMany(attachments: QuestionAttachment[]) {
+    const questionAttachments = this.items.filter((item) => {
+      return !attachments.some((attachment) => attachment.equals(item))
+    })
+
+    this.items = questionAttachments
+  }
+
   async deleteManyByQuestionId(questionId: string) {
     const questionAttachments = this.items.filter((attachment) => attachment.questionId.toString() !== questionId)
 
