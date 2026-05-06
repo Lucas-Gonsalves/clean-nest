5dc1998fbb589f516682800b8520db7ce1e88bb7
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-05-06 15:12:42 -0300

    feat(controllers): update edit-answer and create-answer   Now the controller edit-answer and create-answer is able to register the attachments. A test for validation also as created.

--- src/infra/database/prisma/mappers/prisma-answer-attachment-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-answer-attachment-mapper.ts
@@ -1,6 +1,6 @@
 import { UniqueEntityId } from '@/src/core/entities/unique-entity-id'
 import { AnswerAttachment } from '@/src/domain/forum/enterprise/entities/answer-attachment'
-import { Attachment as PrismaAttachment } from '@/src/generated/prisma/client'
+import { Attachment as PrismaAttachment, Prisma } from '@/src/generated/prisma/client'
 
 export class PrismaAnswerAttachmentMapper {
   static toDomain(raw: PrismaAttachment): AnswerAttachment {
@@ -15,5 +15,20 @@ export class PrismaAnswerAttachmentMapper {
       },
       new UniqueEntityId(raw.id),
     )
+  }
+
+  static toPersistenceUpdateMany(attachments: AnswerAttachment[]): Prisma.AttachmentUpdateManyArgs {
+    const attachmentIds = attachments.map((attachment) => attachment.attachmentId.toString())
+
+    return {
+      where: {
+        id: {
+          in: attachmentIds,
+        },
+      },
+      data: {
+        answerId: attachments[0].answerId.toString(),
+      },
+    }
   }
 }
--- src/infra/database/prisma/repositories/prisma-answer-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answer-attachments-repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common'
 
 import { AnswersAttachmentsRepository } from '@/src/domain/forum/application/repositories/answer-attachments-repository'
+import { AnswerAttachment } from '@/src/domain/forum/enterprise/entities/answer-attachment'
 
 import { PrismaAnswerAttachmentMapper } from '../mappers/prisma-answer-attachment-mapper'
 import { PrismaService } from '../prisma.service'
@@ -21,5 +22,30 @@ export class PrismaAnswersAttachmentsRepository implements AnswersAttachmentsRep
 
   async deleteManyByAnswerId(answerId: string) {
     await this.prisma.attachment.deleteMany({ where: { answerId } })
+  }
+
+  async createMany(attachments: AnswerAttachment[]) {
+    if (attachments.length === 0) {
+      return
+    }
+
+    const data = PrismaAnswerAttachmentMapper.toPersistenceUpdateMany(attachments)
+
+    await this.prisma.attachment.updateMany(data)
+  }
+
+  async deleteMany(attachments: AnswerAttachment[]): Promise<void> {
+    if (attachments.length === 0) {
+      return
+    }
+    const attachmentIds = attachments.map((attachment) => attachment.id.toString())
+
+    await this.prisma.attachment.deleteMany({
+      where: {
+        id: {
+          in: attachmentIds,
+        },
+      },
+    })
   }
 }
